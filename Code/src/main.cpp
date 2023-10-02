e2adb6b6b40db4b7f4524d6d357504acc719c631
SimpleJony <jonylovesmile@outlook.com>
2023-10-02 12:12:09 +0800

    [Fix] Delete "connectWifi" function * We have already configured wifi in Blinker,so when we execute "BLINKER_TAST_INIT()" function,it will connect to wifi.If we use one more function to connect to wifi,it will cause conflict,so we delete it.

--- Code/src/main.cpp
+++ Code/src/main.cpp
@@ -3,7 +3,6 @@
 #define BLINKER_ESP_TASK
 #define BLINKER_MIOT_LIGHT
 #include <Arduino.h>
-#include <WiFi.h>
 #include <Blinker.h>
 #undef ARDUINOJSON_DEFINE_PROGMEM_ARRAY
 #include "Draw/DrawThings.h"
@@ -14,7 +13,6 @@
 #include "Tools/config.h"
 
 // 函数声明
-void connectWifi();
 void onBoot();
 
 void setup(){
@@ -28,36 +26,6 @@ void loop(){
     //codeRain();
     //showWeather();
     delay(200);
-}
-
-/*
-    connectWifi函数实现,用来使ESP32连接上网络
-    Date: 2023.6.23
-    Author: Jony
-*/
-void connectWifi(){
-    int i=0;
-    WiFi.mode(WIFI_STA);
-    WiFi.begin(ssid,pswd);
-    drawWifi();
-    while (WiFi.status()!=WL_CONNECTED){
-        delay(1000);
-        i++;
-        if (i==15){
-            Serial.println("Connect to Wifi failed,board will reboot.");
-            delay(1000);
-            ESP.restart();
-        }
-        else{
-            Serial.println("Connecting to Wifi...");
-        }
-    }
-    drawSuccess();
-    delay(1000);
-    Serial.println("Connected!");
-    Serial.print("IP address:");
-    Serial.print(WiFi.localIP());
-    Serial.println("\n");
 }
 
 /*
@@ -79,7 +47,7 @@ void onBoot(){
     delay(2000);
     matrix.clear();
     delay(1000);
-    connectWifi();
+    //connectWifi();
     configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
     weatherNow.config(UserKey,location_code);
     weatherNow.get();
